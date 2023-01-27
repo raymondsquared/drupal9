3f7002a6b9a9273e46d1eb2edb0b6285d174b1ce
Ray <raymond_squared@live.com>
2023-01-28 01:59:10 +1100

    feat: implements webhook post curl function
    Implements publish method that takes data and url as an input.
    the url is set via env var called WEBHOOK_CONSUMER_URL

--- web/modules/custom/ray_page_changed/src/Controller/PageChangedController.php
+++ web/modules/custom/ray_page_changed/src/Controller/PageChangedController.php
@@ -5,31 +5,44 @@ namespace Drupal\ray_page_changed\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class PageChangedController extends ControllerBase {
-  function publish() {
-    $curl = curl_init();
-    $headers  = [
-      'Content-Type: application/json'
-    ];
-    $postData = [
-      'data1' => 'value1',
-      'data2' => 'value2'
-    ];
-    curl_setopt($curl, CURLOPT_URL,"https://eut0pp2ucd.execute-api.ap-southeast-2.amazonaws.com/test");
-    curl_setopt($curl, CURLOPT_POST, 1);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($postData));           
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    $result     = curl_exec ($curl);
-    $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+  function publish_data($url, $post_data) {
+    try {
+      $curl = curl_init();
+      $headers  = [
+        'Content-Type: application/json'
+      ];
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, 1);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post_data));           
+      curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+      $result     = curl_exec ($curl);
+      $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    } catch (Exception $e) {
+      echo 'Caught exception: ', $e->getMessage(), "\n";
+      $statusCode = 500;
+    }
 
     return $statusCode;
   }
 
   public function page() {
-    $output = publish();
+    $webhook_consumer_url = getenv('WEBHOOK_CONSUMER_URL', true) ?: getenv('WEBHOOK_CONSUMER_URL');
+    $markup = "Listening to page change events. URL: $webhook_consumer_url";
 
+    if($webhook_consumer_url == false) {
+      $markup = "Invalid WEBHOOK_CONSUMER_URL!";
+    } else {
+      $post_data = [
+        'data1' => 'value1',
+        'data2' => 'value2'
+      ];
+      $publish_output = $this->publish_data($webhook_consumer_url, $post_data);
+      $markup = "${markup}, publish_output status code: $publish_output";
+    }
+    
     return array(
-      '#markup' => 'Welcome to our Website. Output = '  + $output
+      '#markup' => $markup
     );
   }
 }
